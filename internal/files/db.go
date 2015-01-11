c6f71d42c015619fed59f219f5fa823efe9a0252
Jakob Borg <jakob@nym.se>
2015-01-11 22:34:38 +0100

    hax

---
+++ internal/files/db.go
@@ -0,0 +1,496 @@
+// Copyright (C) 2014 Jakob Borg and other contributors. All rights reserved.
+// Use of this source code is governed by an MIT-style license that can be
+// found in the LICENSE file.
+
+package files
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/syncthing/syncthing/internal/lamport"
+	"github.com/syncthing/syncthing/internal/protocol"
+)
+
+var setup = []string{
+	`PRAGMA journal_mode = OFF`,
+	`PRAGMA locking_mode = EXCLUSIVE`,
+	`PRAGMA journal_mode = WAL`,
+	`PRAGMA synchronous = NORMAL`,
+	`PRAGMA foreign_keys = ON`,
+}
+
+var schema = []string{
+	`CREATE TABLE IF NOT EXISTS File (
+		ID INTEGER PRIMARY KEY AUTOINCREMENT,
+		Node STRING NOT NULL,
+		Repo STRING NOT NULL,
+		Name STRING NOT NULL,
+		Flags INTEGER NOT NULL,
+		Modified INTEGER NOT NULL,
+		Version INTEGER NOT NULL,
+		Updated BOOLEAN NOT NULL
+	)`,
+	`CREATE UNIQUE INDEX IF NOT EXISTS NodeRepoNameIdx ON File (Node, Repo, Name)`,
+	`CREATE TABLE IF NOT EXISTS Block (
+		Hash BLOB NOT NULL,
+		FileID INTEGER NOT NULL REFERENCES File(ID) ON DELETE CASCADE,
+		Size INTEGER NOT NULL,
+		Offs INTEGER NOT NULL
+	)`,
+	`CREATE INDEX IF NOT EXISTS HashIdx ON Block (Hash)`,
+	`CREATE INDEX IF NOT EXISTS FileIDIdx ON Block (FileID)`,
+}
+
+var preparedStmts = [][2]string{
+	{"selectFileID", "SELECT ID, Version FROM File WHERE Node==? AND Repo==? AND Name==?"},
+	{"selectFileAll", "SELECT ID, Name, Flags, Modified, Version FROM File WHERE Node==? AND Repo==? AND Name==?"},
+	{"selectFileAllID", "SELECT ID, Name, Flags, Modified, Version FROM File WHERE ID==?"},
+	{"selectFileAllVersion", "SELECT ID, Name, Flags, Modified, Version FROM File WHERE Name==? AND Version==?"},
+	{"deleteFile", "DELETE FROM File WHERE ID==?"},
+	{"updateFile", "UPDATE File SET Updated=1 WHERE ID==?"},
+	{"deleteBlock", "DELETE FROM Block WHERE FileID==?"},
+	{"insertFile", "INSERT INTO File (Node, Repo, Name, Flags, Modified, Version, Updated) VALUES (?, ?, ?, ?, ?, ?, ?, ?, 1)"},
+	{"insertBlock", "INSERT INTO Block VALUES (?, ?, ?, ?)"},
+	{"selectBlock", "SELECT Hash, Size, Offs FROM Block WHERE FileID==?"},
+	{"selectFileHave", "SELECT ID, Name, Flags, Modified, Version FROM File WHERE Node==? AND Repo==?"},
+	{"selectFileGlobal", "SELECT ID, Name, Flags, Modified, MAX(Version) FROM File WHERE Repo==? GROUP BY Name ORDER BY Name"},
+	{"selectMaxID", "SELECT MAX(ID) FROM File WHERE Node==? AND Repo==?"},
+	{"selectGlobalID", "SELECT MAX(ID) FROM File WHERE Repo==? AND Name==?"},
+	{"selectMaxVersion", "SELECT MAX(Version) FROM File WHERE Repo==? AND Name==?"},
+	{"selectWithVersion", "SELECT Node, Flags FROM File WHERE Repo==? AND Name==? AND Version==?"},
+	{"selectNeed", "SELECT Name, MAX(Version) Version FROM File WHERE Repo==? GROUP BY Name EXCEPT SELECT Name, Version FROM File WHERE Node==? AND Repo==?"},
+}
+
+type fileDB struct {
+	db    *sql.DB
+	repo  string
+	stmts map[string]*sql.Stmt
+}
+
+func newFileDB(repo, name string) (*fileDB, error) {
+	db, err := sql.Open("sqlite3", "file:"+name+"?cache=shared&mode=rwc")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, stmt := range setup {
+		if debug {
+			l.Debugln(repo, stmt)
+		}
+		_, err = db.Exec(stmt)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, stmt := range schema {
+		if debug {
+			l.Debugln(repo, stmt)
+		}
+		_, err = tx.Exec(stmt)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if err = tx.Commit(); err != nil {
+		return nil, err
+	}
+
+	fdb := fileDB{
+		db:    db,
+		repo:  repo,
+		stmts: make(map[string]*sql.Stmt),
+	}
+
+	for _, prep := range preparedStmts {
+		stmt, err := db.Prepare(prep[1])
+		if debug {
+			l.Debugln(repo, prep[1])
+		}
+		if err != nil {
+			return nil, err
+		}
+		fdb.stmts[prep[0]] = stmt
+	}
+
+	return &fdb, nil
+}
+
+func (db *fileDB) update(device protocol.DeviceID, fs []protocol.FileInfo) error {
+	tx, err := db.db.Begin()
+	if err != nil {
+		l.Fatalln(err)
+	}
+
+	db.updateTx(device, fs, tx)
+
+	return tx.Commit()
+}
+
+func (db *fileDB) updateTx(device protocol.DeviceID, fs []protocol.FileInfo, tx *sql.Tx) error {
+	for _, f := range fs {
+		var id int64
+		var version uint64
+
+		row := tx.Stmt(db.stmts["selectFileID"]).QueryRow(device, db.repo, f.Name)
+		err := row.Scan(&id, &version)
+
+		if err == nil && version != f.Version {
+			_, err = tx.Stmt(db.stmts["deleteFile"]).Exec(id)
+			if err != nil {
+				l.Fatalln(err)
+			}
+		} else if err == nil && version == f.Version {
+			_, err = tx.Stmt(db.stmts["updateFile"]).Exec(id)
+			if err != nil {
+				l.Fatalln(err)
+			}
+		} else if err != nil && err != sql.ErrNoRows {
+			l.Fatalln(err)
+		}
+
+		if version != f.Version {
+			rs, err := tx.Stmt(db.stmts["insertFile"]).Exec(device, db.repo, f.Name, f.Flags, f.Modified, f.Version)
+			if err != nil {
+				l.Fatalln(err)
+			}
+			id, _ = rs.LastInsertId()
+
+			for _, b := range f.Blocks {
+				_, err = tx.Stmt(db.stmts["insertBlock"]).Exec(b.Hash, id, b.Size, b.Offset)
+				if err != nil {
+					l.Fatalln(err)
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func (db *fileDB) updateWithDelete(device protocol.DeviceID, fs []protocol.FileInfo) error {
+	tx, err := db.db.Begin()
+	if err != nil {
+		l.Fatalln(err)
+	}
+
+	_, err = tx.Exec("UPDATE File SET Updated==0 WHERE Node==? AND Repo==?", device, db.repo)
+	if err != nil {
+		l.Fatalln(err)
+	}
+
+	db.updateTx(device, fs, tx)
+
+	rows, err := tx.Query("SELECT ID, Flags, Version FROM File WHERE Repo==? AND Node==? AND Updated==0", db.repo, device.String())
+	if err != nil && err != sql.ErrNoRows {
+		l.Fatalln(err)
+	}
+	for rows.Next() {
+		var id, version uint64
+		var flags uint32
+		err := rows.Scan(&id, &flags, &version)
+		if err != nil {
+			l.Fatalln(err)
+		}
+		if flags&protocol.FlagDeleted == 0 {
+			flags |= protocol.FlagDeleted
+			_, err = tx.Exec("UPDATE File SET Flags=?, Version=? WHERE ID==?", flags, lamport.Default.Tick(version), id)
+			if err != nil {
+				l.Fatalln(err)
+			}
+		}
+	}
+
+	return tx.Commit()
+}
+
+func (db *fileDB) replace(device protocol.DeviceID, fs []protocol.FileInfo) error {
+	tx, err := db.db.Begin()
+	if err != nil {
+		l.Fatalln(err)
+	}
+
+	db.replaceTx(device, fs, tx)
+
+	return tx.Commit()
+}
+
+func (db *fileDB) replaceTx(device protocol.DeviceID, fs []protocol.FileInfo, tx *sql.Tx) error {
+	_, err := tx.Exec("UPDATE File SET Updated==0 WHERE Node==? AND Repo==?", device.String(), db.repo)
+	if err != nil {
+		l.Fatalln(err)
+	}
+
+	for _, f := range fs {
+		var id int64
+		var version uint64
+
+		row := tx.Stmt(db.stmts["selectFileID"]).QueryRow(device, db.repo, f.Name)
+		err := row.Scan(&id, &version)
+
+		if err == nil && version != f.Version {
+			_, err = tx.Stmt(db.stmts["deleteFile"]).Exec(id)
+			if err != nil {
+				l.Fatalln(err)
+			}
+		} else if err != nil && err != sql.ErrNoRows {
+			l.Fatalln(err)
+		}
+
+		if version != f.Version {
+			rs, err := tx.Stmt(db.stmts["insertFile"]).Exec(device, db.repo, f.Name, f.Flags, f.Modified, f.Version)
+			if err != nil {
+				l.Fatalln(err)
+			}
+			id, _ = rs.LastInsertId()
+
+			for _, b := range f.Blocks {
+				_, err = tx.Stmt(db.stmts["insertBlock"]).Exec(b.Hash, id, b.Size, b.Offset)
+				if err != nil {
+					l.Fatalln(err)
+				}
+			}
+		}
+	}
+
+	_, err = tx.Exec("DELETE FROM File WHERE Repo==? AND Node==? AND Updated==0", db.repo, device)
+	if err != nil {
+		l.Fatalln(err)
+	}
+
+	return nil
+}
+
+func (db *fileDB) have(device protocol.DeviceID) []protocol.FileInfo {
+	rows, err := db.stmts["selectFileHave"].Query(device.String(), db.repo)
+	if err == sql.ErrNoRows {
+		return nil
+	}
+	if err != nil {
+		l.Fatalln(err)
+	}
+
+	var files []protocol.FileInfo
+	for rows.Next() {
+		var f protocol.FileInfo
+		var id int64
+		err = rows.Scan(&id, &f.Name, &f.Flags, &f.Modified, &f.Version)
+		if err != nil {
+			l.Fatalln(err)
+		}
+
+		brows, err := db.stmts["selectBlock"].Query(id)
+		if err != nil && err != sql.ErrNoRows {
+			l.Fatalln(err)
+		}
+
+		for brows.Next() {
+			var b protocol.BlockInfo
+			brows.Scan(&b.Hash, &b.Size, &b.Offset)
+			f.Blocks = append(f.Blocks, b)
+		}
+
+		files = append(files, f)
+	}
+
+	return files
+}
+
+func (db *fileDB) global() []protocol.FileInfo {
+	rows, err := db.stmts["selectFileGlobal"].Query(db.repo)
+	if err == sql.ErrNoRows {
+		return nil
+	}
+	if err != nil {
+		l.Fatalln(err)
+	}
+
+	var files []protocol.FileInfo
+	for rows.Next() {
+		var f protocol.FileInfo
+		var id int64
+		err = rows.Scan(&id, &f.Name, &f.Flags, &f.Modified, &f.Version)
+		if err != nil {
+			l.Fatalln(err)
+		}
+
+		brows, err := db.stmts["selectBlock"].Query(id)
+		if err != nil && err != sql.ErrNoRows {
+			l.Fatalln(err)
+		}
+
+		for brows.Next() {
+			var b protocol.BlockInfo
+			brows.Scan(&b.Hash, &b.Size, &b.Offset)
+			f.Blocks = append(f.Blocks, b)
+		}
+
+		files = append(files, f)
+	}
+
+	return files
+}
+
+func (db *fileDB) need(device protocol.DeviceID) []protocol.FileInfo {
+	rows, err := db.stmts["selectNeed"].Query(db.repo, device.String(), db.repo)
+	if err == sql.ErrNoRows {
+		return nil
+	}
+	if err != nil {
+		l.Fatalln(err)
+	}
+
+	var files []protocol.FileInfo
+	for rows.Next() {
+		var name string
+		var version uint64
+		var id int64
+		err = rows.Scan(&name, &version)
+		if err != nil {
+			l.Fatalln(err)
+		}
+
+		var f protocol.FileInfo
+		row := db.stmts["selectFileAllVersion"].QueryRow(name, version)
+		err = row.Scan(&id, &f.Name, &f.Flags, &f.Modified, &f.Version)
+		if err != nil {
+			l.Fatalln(err)
+		}
+
+		brows, err := db.stmts["selectBlock"].Query(id)
+		if err != nil && err != sql.ErrNoRows {
+			l.Fatalln(err)
+		}
+
+		for brows.Next() {
+			var b protocol.BlockInfo
+			brows.Scan(&b.Hash, &b.Size, &b.Offset)
+			f.Blocks = append(f.Blocks, b)
+		}
+
+		files = append(files, f)
+	}
+
+	return files
+}
+
+func (db *fileDB) get(device protocol.DeviceID, name string) protocol.FileInfo {
+	var f protocol.FileInfo
+	var id int64
+
+	row := db.stmts["selectFileAll"].QueryRow(device.String(), db.repo, name)
+	err := row.Scan(&id, &f.Name, &f.Flags, &f.Modified, &f.Version)
+	if err == sql.ErrNoRows {
+		return f
+	}
+	if err != nil {
+		l.Fatalln(err)
+	}
+
+	brows, err := db.stmts["selectBlock"].Query(id)
+	if err != nil && err != sql.ErrNoRows {
+		l.Fatalln(err)
+	}
+
+	for brows.Next() {
+		var b protocol.BlockInfo
+		brows.Scan(&b.Hash, &b.Size, &b.Offset)
+		f.Blocks = append(f.Blocks, b)
+	}
+
+	return f
+}
+
+func (db *fileDB) getGlobal(name string) protocol.FileInfo {
+	var f protocol.FileInfo
+	var gid *uint64
+
+	row := db.stmts["selectGlobalID"].QueryRow(db.repo, name)
+	err := row.Scan(&gid)
+	if gid == nil {
+		return f
+	}
+	if err != nil {
+		l.Fatalln(err)
+	}
+
+	var id uint64
+	row = db.stmts["selectFileAllID"].QueryRow(*gid)
+	err = row.Scan(&id, &f.Name, &f.Flags, &f.Modified, &f.Version)
+	if err == sql.ErrNoRows {
+		return f
+	}
+	if err != nil {
+		l.Fatalln(err)
+	}
+
+	brows, err := db.stmts["selectBlock"].Query(id)
+	if err != nil && err != sql.ErrNoRows {
+		l.Fatalln(err)
+	}
+
+	for brows.Next() {
+		var b protocol.BlockInfo
+		brows.Scan(&b.Hash, &b.Size, &b.Offset)
+		f.Blocks = append(f.Blocks, b)
+	}
+
+	return f
+}
+
+func (db *fileDB) maxID(device protocol.DeviceID) uint64 {
+	var id *uint64
+
+	row := db.stmts["selectMaxID"].QueryRow(device.String(), db.repo)
+	err := row.Scan(&id)
+	if id == nil {
+		return 0
+	}
+	if err != nil {
+		l.Fatalln(err)
+	}
+	return *id
+}
+
+func (db *fileDB) availability(name string) []protocol.DeviceID {
+	var version *int64
+	row := db.stmts["selectMaxVersion"].QueryRow(db.repo, name)
+	err := row.Scan(&version)
+	if version == nil {
+		return nil
+	}
+	if err != nil {
+		l.Fatalln(err)
+	}
+
+	rows, err := db.stmts["selectWithVersion"].Query(db.repo, name, *version)
+	if err == sql.ErrNoRows {
+		return nil
+	}
+	if err != nil {
+		l.Fatalln(err)
+	}
+
+	var available []protocol.DeviceID
+	var node string
+	var flags uint32
+	for rows.Next() {
+		err = rows.Scan(&node, &flags)
+		if err != nil {
+			l.Fatalln(err)
+		}
+		if flags&(protocol.FlagDeleted|protocol.FlagInvalid) == 0 {
+			devID, _ := protocol.DeviceIDFromString(node)
+			available = append(available, devID)
+		}
+	}
+
+	return available
+}
